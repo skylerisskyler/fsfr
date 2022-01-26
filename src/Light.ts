20fd65cf34f9b81308893bdc5393c0971df20b8f
skylerisskyler <riley.skyler@gmail.com>
2022-01-25 23:02:11 -0700

    script improvments

--- src/Light.ts
+++ src/Light.ts
@@ -20,7 +20,7 @@ export class Light {
     variables: Variable[],
     styles: Style[],
     scenes: Scene[],
-    layers: Layer[]
+    layers: Layer[],
   )
   {
 
@@ -52,7 +52,7 @@ export class Light {
           variables,
           styles,
           scenes
-          )
+        )
         layer.lights.push(this)
         this.layers.push(layer)
       }
@@ -68,6 +68,9 @@ export class Light {
     }
   }
 
+  get default() {
+    return false
+  }
 
   getNextScene(currentScene: Scene): Scene | undefined {
     const layerIdx = this.layers.findIndex(layer => currentScene.id === layer.scene.id)
@@ -82,8 +85,8 @@ export class Light {
 
     const scripts: Script[] = []
 
-    scripts.push(...createInfCheckScripts(this))
-    scripts.push(...createSuperiorChecksScripts(this))
+    scripts.push(...createInfHandlerScripts(this))
+    scripts.push(...createSuperiorHandlerScripts(this))
     scripts.push(createSuperiorSceneOnListener(this))
     scripts.push(createListenInfSceneOnScript(this))
     scripts.push(...lightSceneApplyScripts(this))
@@ -92,20 +95,397 @@ export class Light {
   }
 }
 
+//////////////////////////////
+//////////////////////////////
+/////////////////////////////
+//////////////////////////////
+
+
 const getInfSceneOnListenerId = (light: Light) => `${light.id}_inf_scene_on_listener`
 const getInfSceneOffListenerId = (light: Light) => `${light.id}_inf_scene__off_listener`
-const getCurrSceneOffListenerId = (light: Light) => `${light.id}_curr_scene__off_listener`
+const getInfCurrSceneOffListenerId = (light: Light) => `${light.id}_curr_scene__off_listener`
+
+const getRemoveLightFromVarsScriptId = (light: Light, Variable: Variable) => ``
+const getAddLightToVarsScriptId = (light: Light, Variable: Variable) => `add_light_to`
+
 
 const FIRST_INF_SCENE_SCRIPT = "first_inf_scene_script"
 const CURRENT_SCENE_TOGGLE_ID_VAR_NAMESPACE = "current_scene_toggle_id"
+const INF_SCENE_TOGGLE_ID_VAR_NAMESPACE = "inf_scene_toggle_id"
+const APPLY_SCENE_SCRIPT_ID = "apply_scene_script_id"
+const ATTACH_VARS_IN_SCENE_ID = "attach_vars_in_scene_id"
+const DETACH_VARS_IN_SCENE_ID = "detach_vars_in_scene_id"
+
+const infSceneLoopVariables = {
+  [CURRENT_SCENE_TOGGLE_ID_VAR_NAMESPACE]: `{{ ${CURRENT_SCENE_TOGGLE_ID_VAR_NAMESPACE} }}`,
+  [FIRST_INF_SCENE_SCRIPT]: `{{ ${FIRST_INF_SCENE_SCRIPT} }}`
+}
 
 const getSuperiorScriptListenerId = (light: Light) => `${light.id}_sup_scene_listener`
-const getInferiorCheckScriptId = (light: Light, scene: Scene) => `check_inf_${light.id}_${scene.id}`
+const getInferiorHandlerScriptId = (light: Light, scene: Scene) => `handle_inf_${light.id}_${scene.id}`
+const getSuperiorHandlerScriptId = (light: Light, scene: Scene) => `handle_sup_${light.id}_${scene.id}`
 const getApplySceneToLightScriptId = (scene: Scene, light: Light) => `apply_scene_${scene.id}_to_light_${light.id}`
-const getInitInfSceneCheckId = (light: Light) => `init_inf_scene_checks_${light.id}`
-const getInfSceneCheckId = (light: Light) => `init_inf_scene_checks_${light.id}`
+const getInitInfSceneHandlerId = (light: Light) => `init_inf_scene_handles_${light.id}`
+const getInfSceneHandlerId = (light: Light) => `init_inf_scene_handles_${light.id}`
+const getDefaultId = (light: Light) => `default_layer_${light.id}`
+const getTurnOffInfListenersPassthrough = (light: Light)  => `passthrough_${light.id}`
+const getVarAttachScriptId = (light: Light, scene: Scene) => `ATTACH LIGHT TO VARIABLES IN layer`
+const getVarDetachScriptId = (light: Light, scene: Scene) => `DETACH LIGHT FROM VARIABLES IN Layer`
 
-function createSuperiorChecksScripts(light: Light) {
+const toScriptEntityId = (id: string) => `script.${id}`
+
+function createInfHandlerScripts(light: Light) {
+
+  const scripts: Script[] =  light.layers
+    .slice(1)
+    .map((layer, idx, layers) => {
+
+      const { scene } = layer
+
+      const script: Script = new Script({
+        id: getInferiorHandlerScriptId(light, scene),
+        alias: 'some alias'
+      })
+
+      const onChoice = new ChooseActionChoice(`if ${scene.id} is on`)
+        .addCondition({
+          alias: `CONDITION: check ${scene.id} is on`,
+          condition: 'state',
+          entity_id: 'input_boolean.' + getSceneToggleId(scene),
+          state: 'on'
+        })
+        .addAction({
+          alias: `ACTION: initialize current scene off listener`,
+          service: 'script.turn_on',
+          target: {entity_id: getInfCurrSceneOffListenerId(light)},
+          data: {
+            variables: {
+              ...infSceneLoopVariables,
+              callback: getApplySceneToLightScriptId(scene, light)
+            }
+          }
+        })
+        .addAction({
+          alias: `ACTION: initialize ${scene.id} scene off listener`,
+          service: 'script.turn_on',
+          target: {entity_id: getInfSceneOffListenerId(light)},
+          data: {
+            variables: {
+              ...infSceneLoopVariables
+              // what other variables should we pass through?
+            }
+          }
+        })
+
+      const offChoice = new ChooseActionChoice(`if ${scene.id} is off`)
+      .addCondition({
+        condition: 'state',
+        entity_id: 'input_boolean.' + getSceneToggleId(scene),
+        state: 'off'
+      })
+      .addAction({
+        alias: "Initialize this scene on listener",
+        service: 'script.turn_on',
+        target: {entity_id: getInfSceneOnListenerId(light) }, //add to script
+        data: {
+          variables: {
+            ...infSceneLoopVariables,
+            [FIRST_INF_SCENE_SCRIPT]: FIRST_INF_SCENE_SCRIPT,
+            [CURRENT_SCENE_TOGGLE_ID_VAR_NAMESPACE]: CURRENT_SCENE_TOGGLE_ID_VAR_NAMESPACE,
+          }
+        }
+      })
+
+    const nextLayer: Layer | undefined = layers[idx + 1]
+
+    if(nextLayer) {
+
+      offChoice.addAction({
+        service: 'script.turn_on',
+        target: {entity_id: toScriptEntityId(getInferiorHandlerScriptId(light, nextLayer.scene))},
+        data: {
+          variables: {
+            ...infSceneLoopVariables,
+          }
+        }
+      })
+    } else {
+      offChoice.addAction({
+        service: 'script.turn_on',
+        target: {entity_id: toScriptEntityId(getDefaultId(light))},
+      })
+    }
+
+    script
+      .addAction(
+        new ChooseAction('Is inferior scene on or off?')
+        .addChoice(offChoice)
+        .addChoice(onChoice)
+      )
+
+    return script
+  })
+
+  const defaultScript: Script = new Script({
+    id: getDefaultId(light),
+    alias: 'Default layer'
+  })
+
+  if(!light.default) {
+    defaultScript.addAction({
+      service: 'light.turn_off',
+      target: {entity_id: light.entityId}
+    })
+  }
+    
+  scripts.push(defaultScript)
+
+  return scripts
+}
+
+
+
+
+function createListenInfSceneOnScript(light: Light) {
+
+  const script: Script = new Script({
+    id: getInfSceneOnListenerId(light),
+    alias: 'SCRIPT: Listen for inf scene on'
+  })
+  .addAction({
+    wait_template: `{{ is_state(${INF_SCENE_TOGGLE_ID_VAR_NAMESPACE}, 'on') }}`
+  })
+  .addAction({
+    service: 'script.turn_on',
+    target: {entity_id: getInitInfSceneHandlerId(light)},
+    data: {
+      variables: {
+        [FIRST_INF_SCENE_SCRIPT]: FIRST_INF_SCENE_SCRIPT,
+        [CURRENT_SCENE_TOGGLE_ID_VAR_NAMESPACE]: `{{ ${CURRENT_SCENE_TOGGLE_ID_VAR_NAMESPACE} }}`
+      }
+    }
+  })
+
+  return script
+}
+
+function createListenInfSceneOffScript(light: Light) {
+
+  const script: Script = new Script({
+    id: getInfSceneOffListenerId(light),
+    alias: 'some alias',
+    mode: 'parallel'
+  })
+  .addAction({
+    wait_template: "{{ is_state(scene_toggle_id, 'off') }}"
+  })
+  .addAction({
+    service: 'script.turn_on',
+    target: {entity_id: getInitInfSceneHandlerId(light)},
+    data: {
+      variables: {
+        [FIRST_INF_SCENE_SCRIPT]: FIRST_INF_SCENE_SCRIPT,
+        [CURRENT_SCENE_TOGGLE_ID_VAR_NAMESPACE]: `{{ ${CURRENT_SCENE_TOGGLE_ID_VAR_NAMESPACE} }}`
+      }
+    }
+  })
+
+  return script
+}
+
+
+
+function createTurnOffInfListenersPassthrough(light: Light) {
+
+  const script: Script = new Script({
+    id: getTurnOffInfListenersPassthrough(light),
+    alias: 'some alias'
+  })
+  .addAction({
+    alias: "ACTION: Turn off inferior scene on listener",
+    service: "script.turn_off",
+    target: {
+      entity_id: toScriptEntityId(getInfSceneOnListenerId(light))
+    }
+  })
+  .addAction({
+    alias: "ACTION: Turn off inferior scene off listener",
+    service: "script.turn_off",
+    target: {
+      entity_id: toScriptEntityId(getInfSceneOffListenerId(light))
+    }
+  })
+  .addAction({
+    alias: "ACTION: Turn off current scene off listener",
+    service: "script.turn_off",
+    target: {
+      entity_id:  toScriptEntityId(getInfCurrSceneOffListenerId(light))
+    }
+  })
+  .addAction({
+    alias: "ACTION: Call apply scene or scene handler",
+    service: "script.turn_on",
+    target: {
+      entity_id: `{{ call_back }}`
+    },
+    data: {
+      variables: {
+        [FIRST_INF_SCENE_SCRIPT]: FIRST_INF_SCENE_SCRIPT
+      }
+    }
+  })
+
+  return script
+
+}
+
+
+function lightSceneApplyScripts(light: Light) {
+  return light.layers.map((layer, idx, layers) => {
+
+    const { scene } = layer
+
+    const script: Script = new Script({
+      id: getApplySceneToLightScriptId(scene, light),
+      alias: `SCRIPT: Apply scene ${scene.id} to light`
+    })
+    .addAction({
+      alias: "ACTION: Turn off superior scene on listener",
+      service: "script.turn_off",
+      target: {
+        entity_id: toScriptEntityId(getSuperiorScriptListenerId(light))
+      }
+    })
+    .addAction({
+      alias: `ACTION: Detach variables of ${light} in ${scene}`,
+      service: `{{ ${DETACH_VARS_IN_SCENE_ID} }}`,
+    })
+    .addAction({
+      alias: `ACTION: Attach variables of ${light} in ${scene}`,
+      service: `{{ ${ATTACH_VARS_IN_SCENE_ID} }}`,
+    })
+    .addAction({
+      alias: `ACTION: turn on light ${light.id} with data of ${scene}`,
+      service: "light.turn_on",
+      target: {entity_id: light.entityId},
+      data: layer.style.data
+    })
+
+    const firstSuperiorLayer: Layer | undefined = layers[idx +1]
+
+    if(firstSuperiorLayer) {
+      script.addAction({
+        alias: "ACTION: Turn on the first inferior scene handler",
+        service: "script.turn_on",
+        target: {entity_id: toScriptEntityId(getSuperiorHandlerScriptId(light, firstSuperiorLayer.scene))},
+        data: {
+          variables: {
+            [CURRENT_SCENE_TOGGLE_ID_VAR_NAMESPACE]: getSceneToggleId(scene),
+            [FIRST_INF_SCENE_SCRIPT]: getInferiorHandlerScriptId(light, scene)
+          }
+        }
+      })
+    }
+
+    const firstInferiorLayer: Layer | undefined = layers[idx - 1]
+
+    if(firstInferiorLayer) {
+      script.addAction({
+        alias: `ACTION: Turn on the first inferior scene handler`,
+        service: "script.turn_on",
+        target: {entity_id: toScriptEntityId(getInferiorHandlerScriptId(light, firstInferiorLayer.scene))},
+        data: {
+          variables: {
+            CURRENT_SCENE_TOGGLE_ID_VAR_NAMESPACE, //this is prob not it
+            next_inf_scene_script: getInferiorHandlerScriptId
+          }
+        }
+      })
+    }
+
+    return script
+  })
+}
+
+
+
+
+
+
+
+
+
+function createVarAttachScripts(light: Light): Script[] {
+
+  return light.layers.map(layer => {
+    
+    const { scene } = layer
+
+    const script: Script = new Script({
+      id: getVarAttachScriptId(light, scene),
+      alias: 'some alias'
+    })
+
+    layer.style.variables.forEach((variable: Variable) => {
+      script.addAction({
+        service: 'script.turn_on',
+        target: {entity_id: `script.util_add_light_to_var`},
+        data: {
+          variables: {
+            light_to_add: light.entityId
+          }
+        }
+      })
+    })
+
+    return script
+    
+  })
+}
+
+function createVarDetachScripts(light: Light): Script[] {
+
+  return light.layers.map(layer => {
+    
+    const { scene } = layer
+
+    const script: Script = new Script({
+      id: getVarDetachScriptId(light, scene),
+      alias: 'some alias'
+    })
+
+    layer.style.variables.forEach((variable: Variable) => {
+      script.addAction({
+        service: 'script.turn_on',
+        target: {entity_id: `script.util_remove_light_from_var`},
+        data: {
+          variables: {
+            light_to_remove: light.entityId
+          }
+        }
+      })
+    })
+
+    return script
+    
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function createSuperiorHandlerScripts(light: Light) {
 
   const scripts: Script[] =  light.layers
     .reverse()
@@ -115,7 +495,7 @@ function createSuperiorChecksScripts(light: Light) {
       const { scene } = layer
 
       const script: Script = new Script({
-        id: `sup_${light.id}_${scene.id}_check`,
+        id: `sup_${light.id}_${scene.id}_handle`,
         alias: 'some alias'
       })
       .addAction({
@@ -125,89 +505,6 @@ function createSuperiorChecksScripts(light: Light) {
           variables: {}
         }
       })
-
-      return script
-    })
-
-  return scripts
-}
-
-function createInfCheckScripts(light: Light) {
-
-  const scripts: Script[] =  light.layers
-    .slice(1)
-    .map((layer, idx, layers) => {
-
-      const { scene } = layer
-
-      const script: Script = new Script({
-        id: getInferiorCheckScriptId(light, scene),
-        alias: 'some alias'
-      })
-
-      const offChoice = new ChooseActionChoice(`if ${scene.id} is off`)
-        .addCondition({
-          condition: 'state',
-          entity_id: 'input_boolean.' + getSceneToggleId(scene),
-          state: 'off'
-        })
-        .addAction({
-          alias: "Initialize this scene on listener",
-          service: 'script.turn_on',
-          target: {entity_id: getInfSceneOnListenerId(light) },
-          data: {
-            variables: {
-              scene_toggle_id: getSceneToggleId(scene),
-              [FIRST_INF_SCENE_SCRIPT]: `{{ ${FIRST_INF_SCENE_SCRIPT} }}`
-            }
-          }
-        })
-
-      const nextLayer: Layer | undefined = layers[idx + 1]
-
-      if(nextLayer) {
-
-        offChoice.addAction({
-          service: 'script.turn_on',
-          target: {entity_id: 'script.' + getInferiorCheckScriptId(light, nextLayer.scene)},
-          data: {
-            variables: {}
-          }
-        })
-
-      }
-
-      const onChoice = new ChooseActionChoice(`if ${scene.id} is on`)
-        .addCondition({
-          condition: 'state',
-          entity_id: 'input_boolean.' + getSceneToggleId(scene),
-          state: 'on'
-        })
-        .addAction({
-          service: 'script.turn_on',
-          target: {entity_id: getCurrSceneOffListenerId(light)},
-          data: {
-            variables: {
-              apply_scene_script_id: getApplySceneToLightScriptId(scene, light)
-            }
-          }
-        })
-        .addAction({
-          service: 'script.turn_on',
-          target: {entity_id: getInfSceneOffListenerId(light)},
-          data: {
-            variables: {
-              apply_scene_script_id: getApplySceneToLightScriptId(scene, light)
-            }
-          }
-        })
-  
-      script
-        .addAction(
-          new ChooseAction('some alias')
-          .addChoice(offChoice)
-          .addChoice(onChoice)
-        )
 
       return script
     })
@@ -233,127 +530,4 @@ function createSuperiorSceneOnListener(light: Light) {
   })
 
   return superiorSceneListener
-}
-
-
-
-function createListenInfSceneOnScript(light: Light) {
-
-  const script: Script = new Script({
-    id: getInfSceneOnListenerId(light),
-    alias: 'some alias'
-  })
-  .addAction({
-    wait_template: "{{ is_state(scene_toggle_id, 'on') }}"
-  })
-  .addAction({
-    service: 'script.turn_on',
-    target: {entity_id: getInitInfSceneCheckId(light)},
-    data: {
-      variables: {
-        [FIRST_INF_SCENE_SCRIPT]: FIRST_INF_SCENE_SCRIPT,
-        [CURRENT_SCENE_TOGGLE_ID_VAR_NAMESPACE]: `{{ ${CURRENT_SCENE_TOGGLE_ID_VAR_NAMESPACE} }}`
-      }
-    }
-  })
-
-  return script
-}
-
-function createListenInfSceneOffScript(light: Light) {
-
-  const script: Script = new Script({
-    id: getInfSceneOffListenerId(light),
-    alias: 'some alias',
-    mode: 'parallel'
-  })
-  .addAction({
-    wait_template: "{{ is_state(scene_toggle_id, 'off') }}"
-  })
-  .addAction({
-    service: 'script.turn_on',
-    target: {entity_id: getInitInfSceneCheckId(light)},
-    data: {
-      variables: {
-        [FIRST_INF_SCENE_SCRIPT]: FIRST_INF_SCENE_SCRIPT,
-        [CURRENT_SCENE_TOGGLE_ID_VAR_NAMESPACE]: `{{ ${CURRENT_SCENE_TOGGLE_ID_VAR_NAMESPACE} }}`
-      }
-    }
-  })
-
-  return script
-}
-
-
-
-function createInitInfSceneChecks(light: Light) {
-
-  const script: Script = new Script({
-    id: getInitInfSceneCheckId(light),
-    alias: 'some alias'
-  })
-  .addAction({
-    service: "script.turn_off",
-    target: {
-      entity_id: 'script.' + getInfSceneOnListenerId(light)
-    }
-  })
-  .addAction({
-    service: "script.turn_off",
-    target: {
-      entity_id: 'script.' + getInfSceneOffListenerId(light)
-    }
-  })
-  .addAction({
-    service: "script.turn_off",
-    target: {
-      entity_id: 'script.' + getCurrSceneOffListenerId(light)
-    }
-  })
-  .addAction({
-    service: "script.turn_on",
-    target: {
-      entity_id: `{{ ${CURRENT_SCENE_TOGGLE_ID_VAR_NAMESPACE} }}`
-    }
-  })
-
-  return script
-
-}
-
-function lightSceneApplyScripts(light: Light) {
-  return light.layers.map((layer) => {
-
-    const script: Script = new Script({
-      id: getApplySceneToLightScriptId(layer.scene, light),
-      alias: 'some alias'
-    })
-    .addAction({
-      service: "script.turn_off",
-      target: {
-        entity_id: 'script.' + getSuperiorScriptListenerId(light)
-      }
-    })
-
-    layer.style.variables.forEach((variable: Variable) => {
-      script.addAction({
-        service: 'script.turn_on',
-        target: {entity_id: `script.util_rmv_light_scene`},
-        data: {
-          variables: {
-            var_namespace: variable.namespace,
-            scene_id: layer.scene.id
-          }
-        }
-      })
-    })
-
-    script.addAction({
-      service: "light.turn_on",
-      target: {entity_id: light.entityId},
-      data: layer.style.data
-    })
-    //add this stuff man
-    return script
-  })
 }
